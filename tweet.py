ad862f4cdd211925d425d2836c59ab90bde7bdd7
Mohit Solanki <mohitsolanki619@gmail.com>
2018-06-13 11:33:50 +0530

    Update tweet.py

--- tweet.py
+++ tweet.py
@@ -11,7 +11,7 @@ logging.basicConfig(filename='errors.log', level=logging.INFO)
 def construct_tweet(event):
     event_tweet = '{} on {} at {} more details at {}'.format(
         event['name'].strip(),
-        datetime.strptime(event['local_date'], "%Y-%m-%d").strftime("%-m %B"),
+        datetime.strptime(event['local_date'], "%Y-%m-%d").strftime("%-d %B"),
         datetime.strptime(event['local_time'], "%H:%M").strftime("%I:%M %p"),
         event['link'],
     )
